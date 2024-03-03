8b39ec578b372fd16d47941479530bbee4cc317b
khatiachub <101997849+khatiachub@users.noreply.github.com>
2024-03-03 18:14:16 +0400

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter,Route, Routes } from 'react-router-dom'
+import { HashRouter,Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import History from './pages/History'
 
 function App() {
   return (
-    <BrowserRouter basename="/sweeft-task/">
+    <HashRouter >
     <Routes>
       <Route path='/' element={<MainPage/>}/>
       <Route path='/history' element={<History/>}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
