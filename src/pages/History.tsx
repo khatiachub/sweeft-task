ebb84acdcb6699ec9845a1fc3641d2fadf0fe29d
khatiachub <101997849+khatiachub@users.noreply.github.com>
2024-03-03 13:41:28 +0400

    commit

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,18 +1,17 @@
+import {Link} from "react-router-dom";
+import {usestoredData} from "../components/Context";
+import {useEffect, useMemo, useState} from "react";
+import ImageDetails from "../components/ImageDetails";
+import axios from "axios";
 
-import { Link } from 'react-router-dom'
-import { usestoredData } from '../components/Context';
-import { useEffect, useMemo, useState } from 'react';
-import ImageDetails from '../components/ImageDetails';
-import axios from 'axios';
-
-interface DataType{
-  queryKey:any[]
+interface DataType {
+  queryKey: any[];
 }
 interface Image {
   id: number;
-  urls:{
-    small:string
-  }
+  urls: {
+    small: string;
+  };
 }
 interface Results {
   results: Image[];
@@ -20,131 +19,156 @@ interface Results {
 interface Item {
   state: {
     data: Results;
-  }
+  };
 }
-interface Statistics{
-  downloads:{
-      total:number
-  },
-  likes:{
-      total:number
-  },
-  views:{
-      total:number
-  }
+interface Statistics {
+  downloads: {
+    total: number;
+  };
+  likes: {
+    total: number;
+  };
+  views: {
+    total: number;
+  };
 }
 export default function History() {
- const{storeData}=usestoredData();
-const[open,setOpen]=useState<string>('')
-const[pageNumber,setPageNumber]=useState(1)
-const[statistics,setStatistics]=useState<Statistics>({} as Statistics)
-const[modalWindow,setModalWindow]=useState(false)
-const[filteredImage,setFilteredImage]=useState<any[]>([])
-const[dataArray,setDataArray]=useState<readonly unknown[]>([])
+  const {storeData} = usestoredData();
+  const [open, setOpen] = useState<string>("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const [statistics, setStatistics] = useState<Statistics>({} as Statistics);
+  const [modalWindow, setModalWindow] = useState(false);
+  const [filteredImage, setFilteredImage] = useState<any[]>([]);
+  const [dataArray, setDataArray] = useState<readonly unknown[]>([]);
 
-const handleScroll = () => {
-  if (
-    window.innerHeight + document.documentElement.scrollTop ===
-    document.documentElement.offsetHeight
-  ) {
-    setPageNumber((prevPageNumber) => prevPageNumber + 1);
-  }
-};
-useEffect(() => {
-  window.addEventListener('scroll', handleScroll);
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setPageNumber((prevPageNumber) => prevPageNumber + 1);
+    }
   };
-}, [handleScroll]);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
 
+  const fetchData = () => {
+    const data =
+      storeData &&
+      storeData.find(
+        (item: DataType) =>
+          item.queryKey[2] === pageNumber && item.queryKey[1] === open
+      );
+    if (pageNumber > 1) {
+      setDataArray((prevDataArray) => [...prevDataArray, data].filter(Boolean));
+    } else {
+      setDataArray([data].filter(Boolean));
+    }
+  };
 
-const fetchData=()=>{
-  const data = storeData &&
-  storeData.find((item:DataType) => item.queryKey[2] === pageNumber&&item.queryKey[1]===open);
-  if(pageNumber>1){
-    setDataArray((prevDataArray) => [...prevDataArray, data].filter(Boolean));
-  }else{
-    setDataArray([data].filter(Boolean));
+  const handleClick = (name: string) => {
+    setDataArray([]);
+    setOpen("");
+    setPageNumber(1);
+    setOpen(name);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [pageNumber, open]);
+
+  function getUniqueCompleteWords(words: any[] | undefined) {
+    const uniqueWords = new Set(words);
+    const completeWords = Array.from(uniqueWords).filter((word, index, arr) => {
+      const restOfWords = arr.slice(0, index).concat(arr.slice(index + 1));
+      return restOfWords.every((otherWord) => !otherWord.includes(word));
+    });
+    return completeWords;
   }
-}
+  const maxword =
+    storeData && storeData.map((item: DataType) => item.queryKey[1]);
+  const filteredMaxWord: any[] | undefined = maxword?.filter(
+    (word) => word !== ""
+  );
+  const uniqueWord = useMemo(
+    () => getUniqueCompleteWords(filteredMaxWord),
+    [filteredMaxWord]
+  );
 
-
-
-const handleClick=(name:string)=>{
-  setDataArray([])
-  setOpen('')
-  setPageNumber(1)
-  setOpen(name)
-}
-
-useEffect(()=>{
-  fetchData()  
-},[pageNumber,open])
-
-
-
-function getUniqueCompleteWords(words:any[]|undefined) {
-  const uniqueWords = new Set(words);
-  const completeWords = Array.from(uniqueWords).filter((word, index, arr) => {
-    const restOfWords = arr.slice(0, index).concat(arr.slice(index + 1));
-    return restOfWords.every(otherWord => !otherWord.includes(word));
-  });
-  return completeWords;
-}
-const maxword=storeData&&storeData.map((item:DataType)=>item.queryKey[1])
-const filteredMaxWord:any[]|undefined= maxword?.filter((word) => word !== '');
-  const uniqueWord = useMemo(() => getUniqueCompleteWords(filteredMaxWord), [filteredMaxWord]);
-
-
-const clickImage=(id:number)=>{
-  const fetchStatData=async()=>{
-      try{
-        const headers={
-          'Authorization':'Client-ID FAfkfh4tINXvU6ajMAjZp9XpBtvp0j_b7zH6VNtD4vs'
-        }
-        const res=await axios.get(`https://api.unsplash.com/photos/${id}/statistics`,{headers})
-        setStatistics(res.data)
-      }
-      catch(err){
+  const clickImage = (id: number) => {
+    const fetchStatData = async () => {
+      try {
+        const headers = {
+          Authorization:
+            "Client-ID FAfkfh4tINXvU6ajMAjZp9XpBtvp0j_b7zH6VNtD4vs",
+        };
+        const res = await axios.get(
+          `https://api.unsplash.com/photos/${id}/statistics`,
+          {headers}
+        );
+        setStatistics(res.data);
+      } catch (err) {
         console.log(err);
       }
-    }
+    };
     fetchStatData();
-    setModalWindow(true)
-    const filteredData=dataArray&&dataArray.map((item)=>((item as Item)?.state?.data?.results)).map((item)=>(item.filter((image)=>(image.id===id))))
-    setFilteredImage(filteredData.flat())
-}
-const closeWindow=()=>{
-  setModalWindow(false)
-}
+    setModalWindow(true);
+    const filteredData =
+      dataArray &&
+      dataArray
+        .map((item) => (item as Item)?.state?.data?.results)
+        .map((item) => item.filter((image) => image.id === id));
+    setFilteredImage(filteredData.flat());
+  };
+  const closeWindow = () => {
+    setModalWindow(false);
+  };
 
-
-
-console.log(filteredMaxWord);
+  console.log(filteredMaxWord);
 
   return (
     <div>
-      {filteredMaxWord?
-      <><nav className='navigation-history'>
-        <Link className='nav-text' to={'/'}>მთავარი</Link>
-        <Link style={{marginLeft:20}} className='nav-text' to={'/history'}>ისტორია</Link>
-        </nav>
-        
-
-        <ul className='list-history'>
-        {uniqueWord.map((item)=>(
-          <li className='list-item' onClick={()=>handleClick(item)}>{item}</li>
+      <nav className="navigation-history">
+        <Link className="nav-text" to={"/"}>
+          მთავარი
+        </Link>
+        <Link style={{marginLeft: 20}} className="nav-text" to={"/history"}>
+          ისტორია
+        </Link>
+      </nav>
+      <ul className="list-history">
+        {uniqueWord.map((item) => (
+          <li className="list-item" onClick={() => handleClick(item)}>
+            {item}
+          </li>
         ))}
-        </ul>
+      </ul>
 
-       <div className='img-wraper'>
-       {dataArray&&dataArray.map((item)=>((item as Item)?.state.data.results.map((image)=>(
-          <img className='image' onClick={()=>clickImage(image.id)}   src={image?.urls?.small}/>
-        ))))
-        }
-       </div>
-    <ImageDetails modalWindow={modalWindow} statistics={statistics} closeWindow={closeWindow} filteredImage={filteredImage}/>
-        </>:<h1 className='history-title'>ისტორია არ მოიძებნა</h1>}
+      <div className="img-wraper">
+        {dataArray &&
+          dataArray.map((item) =>
+            (item as Item)?.state.data.results.map((image) => (
+              <img
+                className="image"
+                onClick={() => clickImage(image.id)}
+                src={image?.urls?.small}
+              />
+            ))
+          )}
+      </div>
+      <ImageDetails
+        modalWindow={modalWindow}
+        statistics={statistics}
+        closeWindow={closeWindow}
+        filteredImage={filteredImage}
+      />
+      {filteredMaxWord?.length === 0 && (
+        <h2 className="history-title">ისტორია არ მოიძებნა!</h2>
+      )}
     </div>
-  )
+  );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,17 +1,17 @@
-import axios from 'axios'
-import  { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import '../css/styled.css'
-import { useQuery, useQueryClient } from 'react-query';
-import { usestoredData } from '../components/Context';
-import ImageDetails from '../components/ImageDetails';
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import "../css/styled.css";
+import {useQuery, useQueryClient} from "react-query";
+import {usestoredData} from "../components/Context";
+import ImageDetails from "../components/ImageDetails";
 
-interface Image{
-  id:number,
-  urls:Urls
+interface Image {
+  id: number;
+  urls: Urls;
 }
-interface Urls{
-  small:string
+interface Urls {
+  small: string;
 }
 interface ApiResponse {
   results: any[];
@@ -19,162 +19,173 @@ interface ApiResponse {
   total_pages: number;
 }
 
-interface Statistics{
-    downloads:{
-        total:number
-    },
-    likes:{
-        total:number
-    },
-    views:{
-        total:number
-    }
+interface Statistics {
+  downloads: {
+    total: number;
+  };
+  likes: {
+    total: number;
+  };
+  views: {
+    total: number;
+  };
 }
 export default function MainPage() {
-    const[keyValue,setKeyValue]=useState<string>('')
-    const[pageNumber,setPageNumber]=useState<number>(1)
-    const[dataArray,setDataArray]=useState<Image[]>([])
-    const[statistics,setStatistics]=useState<Statistics>({} as Statistics)
-    const[modalWindow,setModalWindow]=useState(false)
-    const[filteredImage,setFilteredImage]=useState<Image[]>([])
-    const queryClient = useQueryClient();
-    const {getData}=usestoredData()
-    const allCachedQueries:readonly unknown[] = queryClient.getQueryCache().findAll();
-    getData(allCachedQueries as [])
+  const [keyValue, setKeyValue] = useState<string>("");
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [dataArray, setDataArray] = useState<Image[]>([]);
+  const [statistics, setStatistics] = useState<Statistics>({} as Statistics);
+  const [modalWindow, setModalWindow] = useState(false);
+  const [filteredImage, setFilteredImage] = useState<Image[]>([]);
+  const queryClient = useQueryClient();
+  const {getData} = usestoredData();
+  const allCachedQueries: readonly unknown[] = queryClient
+    .getQueryCache()
+    .findAll();
+  getData(allCachedQueries as []);
 
-
-  
-
-    useEffect(()=>{
-        const fetchData=async()=>{
-          try{
-            const headers={
-              'Authorization':'Client-ID FAfkfh4tINXvU6ajMAjZp9XpBtvp0j_b7zH6VNtD4vs'
-            }
-            const res=await axios.get(`https://api.unsplash.com/photos?per_page=20`,{headers})
-            if(keyValue===''){
-                setDataArray(res.data);
-            }
-          }
-          catch(err){
-            console.log(err);
-          }
-        }
-        fetchData();
-    },[])
-    
-    useEffect(() => {
-      if (!keyValue) {
-        fetchData();
-        
-      }
-    }, [keyValue]);
-
-
-   
-      const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
-        setKeyValue(e.target.value)
-      };
-      
-
-      
-  
-      const handleClick=(id:number)=>{
-        const fetchStatData=async()=>{
-            try{
-              const headers={
-                'Authorization':'Client-ID FAfkfh4tINXvU6ajMAjZp9XpBtvp0j_b7zH6VNtD4vs'
-              }
-              const res=await axios.get(`https://api.unsplash.com/photos/${id}/statistics`,{headers})
-              setStatistics(res.data)
-            }
-            catch(err){
-              console.log(err);
-            }
-          }
-          fetchStatData();
-          setModalWindow(true)
-          setFilteredImage(dataArray.flat().filter((image)=>(image.id===id)))
-      }
-      const closeWindow=()=>{
-        setModalWindow(false)
-      }
-      
-
-
-      const handleScroll = () => {
-        if (
-          window.innerHeight + document.documentElement.scrollTop ===
-          document.documentElement.offsetHeight
-        ) {
-          setPageNumber((prevPageNumber) => prevPageNumber + 1);
-        }
-      };
-      useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const headers = {
+          Authorization:
+            "Client-ID FAfkfh4tINXvU6ajMAjZp9XpBtvp0j_b7zH6VNtD4vs",
         };
-      }, [handleScroll]);
-
-
-      const headers={
-        'Authorization':'Client-ID FAfkfh4tINXvU6ajMAjZp9XpBtvp0j_b7zH6VNtD4vs'
-       }
-     
-      
-       
-       const fetchData = async ():Promise<ApiResponse> => {
-         const res=await axios.get(`https://api.unsplash.com/search/photos?page=${pageNumber}&query=${keyValue}&per_page=20`,{headers})        
-         const data:ApiResponse=res.data      
-         queryClient.setQueryData(['data',keyValue, pageNumber], data);
-         return data;        
-       };
-   
-       
-      
-      const {data,isError}=useQuery<ApiResponse>({
-        queryFn:()=>fetchData(),
-        queryKey:['data',keyValue,pageNumber],
-        staleTime:Infinity
-      })
-      
-      if (isError) {
-        return <p>Error fetching data</p>;
+        const res = await axios.get(
+          `https://api.unsplash.com/photos?per_page=20`,
+          {headers}
+        );
+        if (keyValue === "") {
+          setDataArray(res.data);
+        }
+      } catch (err) {
+        console.log(err);
       }
-      
-    
-      
-      useEffect(() => {
-          if(keyValue!==''){
-            if (pageNumber === 1) {
-              setDataArray(data?.results||[]);
-            } else {
-              setDataArray((prevDataArray) => [...prevDataArray, ...(data?.results || [])]);
-              setDataArray((prevDataArray) => prevDataArray.flat(1));
-            }
-          }
-      }, [keyValue,data, isError, pageNumber]);
-      
+    };
+    fetchData();
+  }, []);
 
+  useEffect(() => {
+    if (!keyValue) {
+      fetchData();
+    }
+  }, [keyValue]);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyValue(e.target.value);
+  };
 
+  const handleClick = (id: number) => {
+    const fetchStatData = async () => {
+      try {
+        const headers = {
+          Authorization:
+            "Client-ID FAfkfh4tINXvU6ajMAjZp9XpBtvp0j_b7zH6VNtD4vs",
+        };
+        const res = await axios.get(
+          `https://api.unsplash.com/photos/${id}/statistics`,
+          {headers}
+        );
+        setStatistics(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchStatData();
+    setModalWindow(true);
+    setFilteredImage(dataArray.flat().filter((image) => image.id === id));
+  };
+  const closeWindow = () => {
+    setModalWindow(false);
+  };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setPageNumber((prevPageNumber) => prevPageNumber + 1);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  const headers = {
+    Authorization: "Client-ID FAfkfh4tINXvU6ajMAjZp9XpBtvp0j_b7zH6VNtD4vs",
+  };
+
+  const fetchData = async (): Promise<ApiResponse> => {
+    const res = await axios.get(
+      `https://api.unsplash.com/search/photos?page=${pageNumber}&query=${keyValue}&per_page=20`,
+      {headers}
+    );
+    const data: ApiResponse = res.data;
+    queryClient.setQueryData(["data", keyValue, pageNumber], data);
+    return data;
+  };
+
+  const {data, isError} = useQuery<ApiResponse>({
+    queryFn: () => fetchData(),
+    queryKey: ["data", keyValue, pageNumber],
+    staleTime: Infinity,
+  });
+
+  if (isError) {
+    return <p>Error fetching data</p>;
+  }
+
+  useEffect(() => {
+    if (keyValue !== "") {
+      if (pageNumber === 1) {
+        setDataArray(data?.results || []);
+      } else {
+        setDataArray((prevDataArray) => [
+          ...prevDataArray,
+          ...(data?.results || []),
+        ]);
+        setDataArray((prevDataArray) => prevDataArray.flat(1));
+      }
+    }
+  }, [keyValue, data, isError, pageNumber]);
 
   return (
     <div>
-    <div className='header-box'>
-    <input className='input' type='text' placeholder='შეიყვანეთ საძიებო სიტყვა' onChange={(e)=>handleChange(e)}/>
-    <nav className='navigation'>
-       <Link className='nav-text' to={'/'}>მთავარი</Link>
-      <Link className='nav-text' to={'/history'}>ისტორია</Link>
-    </nav>
+      <div className="header-box">
+        <input
+          className="input"
+          type="text"
+          placeholder="შეიყვანეთ საძიებო სიტყვა"
+          onChange={(e) => handleChange(e)}
+        />
+        <nav className="navigation">
+          <Link className="nav-text" to={"/"}>
+            მთავარი
+          </Link>
+          <Link className="nav-text" to={"/history"}>
+            ისტორია
+          </Link>
+        </nav>
+      </div>
+      <div className="img-wraper">
+        {dataArray &&
+          dataArray.map((item) => (
+            <img
+              className="image"
+              onClick={() => handleClick(item.id)}
+              src={item?.urls?.small}
+            />
+          ))}
+      </div>
+      <ImageDetails
+        modalWindow={modalWindow}
+        statistics={statistics}
+        closeWindow={closeWindow}
+        filteredImage={filteredImage}
+      />
     </div>
-    <div className='img-wraper'>
-    {dataArray&&dataArray.map((item)=>(
-        <img  className='image'  onClick={()=>handleClick(item.id)} src={item?.urls?.small}/>
-    ))}
-    </div>
-    <ImageDetails modalWindow={modalWindow} statistics={statistics} closeWindow={closeWindow} filteredImage={filteredImage}/>
-    </div>
-  )
+  );
 }
