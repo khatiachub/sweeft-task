ddc62de2188d76f0a60b550e6550f3e89cfc0880
khatiachub <101997849+khatiachub@users.noreply.github.com>
2024-03-03 14:21:38 +0400

    deploy app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {HashRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import History from './pages/History'
 
 function App() {
+ 
   return (
-    <BrowserRouter>
+    <HashRouter >
     <Routes>
       <Route path='/' element={<MainPage/>}/>
       <Route path='/history' element={<History/>}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
